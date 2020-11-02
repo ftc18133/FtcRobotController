765b994084004bdc2a11222ba8cbe9a8da4d26a3
spellingcat <zhenyahu@hotmail.com>
2020-11-01 16:31:54 -0800

    commented out ringmotor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CyberCatBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CyberCatBot.java
@@ -123,9 +123,9 @@ public class CyberCatBot {
         motorR2.setDirection(DcMotorSimple.Direction.REVERSE);
         initMotor(motorR2);
 
-        ringMotor = hardwareMap.get(DcMotorEx.class, "ringMotor");
-        ringMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        ringMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        //ringMotor = hardwareMap.get(DcMotorEx.class, "ringMotor");
+        //ringMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //ringMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         lightSensor = hardwareMap.get(LightSensor.class, "lightSensor");
         lightSensor.enableLed(true);
