f2f136469b3939f2d83d5c4ffb7293fd62dbea82
spellingcat <zhenyahu@hotmail.com>
2021-01-03 13:49:52 -0800

    Tried to fix the driveToSquare code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousJavaOpMode.java
@@ -168,14 +168,21 @@ public class AutonomousJavaOpMode extends LinearOpMode {
             if (square == SQUARE_A) {
                 go(AUTONOMOUS_VELOCITY, (80*INCHES_TO_CM) - (0.75*SQUARE_WIDTH*INCHES_TO_CM), CyberCatBot.FORWARD);
                 go(AUTONOMOUS_VELOCITY, 1.5*SQUARE_WIDTH*INCHES_TO_CM, CyberCatBot.RIGHT);
+                go(AUTONOMOUS_VELOCITY, 1.5*SQUARE_WIDTH*INCHES_TO_CM, CyberCatBot.LEFT);
+                go(AUTONOMOUS_VELOCITY, (80*INCHES_TO_CM) - (0.75*SQUARE_WIDTH*INCHES_TO_CM), CyberCatBot.BACKWARD);
             }
             else if (square == SQUARE_B) {
                 go(AUTONOMOUS_VELOCITY, 102.75*INCHES_TO_CM, CyberCatBot.FORWARD);
-                go(AUTONOMOUS_VELOCITY, 11.375*INCHES_TO_CM, CyberCatBot.LEFT);
+                go(AUTONOMOUS_VELOCITY, 11.375*SQUARE_WIDTH*INCHES_TO_CM, CyberCatBot.LEFT);
+                go(AUTONOMOUS_VELOCITY, 11.375*SQUARE_WIDTH*INCHES_TO_CM, CyberCatBot.RIGHT);
+                go(AUTONOMOUS_VELOCITY, 102.75*INCHES_TO_CM, CyberCatBot.BACKWARD);
             }
             else if (square == SQUARE_C) {
-                go(AUTONOMOUS_VELOCITY, 0.5*SQUARE_WIDTH*INCHES_TO_CM, CyberCatBot.RIGHT);
-                go(AUTONOMOUS_VELOCITY, 125.5*INCHES_TO_CM, CyberCatBot.FORWARD);
+                go(AUTONOMOUS_VELOCITY, 125.5 * INCHES_TO_CM, CyberCatBot.FORWARD);
+                go(AUTONOMOUS_VELOCITY, 0.5 * SQUARE_WIDTH * INCHES_TO_CM, CyberCatBot.RIGHT);
+                go(AUTONOMOUS_VELOCITY, 0.5 * SQUARE_WIDTH * INCHES_TO_CM, CyberCatBot.LEFT);
+                go(AUTONOMOUS_VELOCITY, 125.5 * INCHES_TO_CM, CyberCatBot.BACKWARD);
+
             }
 
         //}
